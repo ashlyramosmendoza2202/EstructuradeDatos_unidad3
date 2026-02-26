d6528a01262cc5f4768f41e48996d565e597744f
ashlyramosmendoza2202 <ashlyramosmendoza2202@gmail.com>
2026-02-26 00:01:23 -0400

    cargando

---
+++ Ejercicio4_Tuplas/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Ingresa el primer número: ");
+        int n1 = Convert.ToInt32(Console.ReadLine());
+
+        Console.Write("Ingresa el segundo número: ");
+        int n2 = Convert.ToInt32(Console.ReadLine());
+
+        int mayor = EncontrarMayor(n1, n2);
+
+        Console.WriteLine($"El número mayor es: {mayor}");
+    }
+    static int EncontrarMayor(int a, int b)
+    {
+        if (a > b)
+        {
+            return a;
+        }
+        else
+        {
+            return b;
+        }
+    }
+}
