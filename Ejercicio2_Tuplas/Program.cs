d1c00c56dc579d74081d600f6c0d03114fa58dae
ashlyramosmendoza2202 <ashlyramosmendoza2202@gmail.com>
2026-02-25 23:43:56 -0400

    cargando

---
+++ Ejercicio2_Tuplas/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Ejercicio01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Ingrese un número para contar los pares hasta ese límite: ");
+            int limite = int.Parse(Console.ReadLine());
+
+            int contadorPares = 0;
+
+
+            for (int i = 1; i <= limite; i++)
+            {
+
+                if (i % 2 == 0)
+                {
+                    contadorPares++;
+                }
+            }
+
+
+            Console.WriteLine("\nEntre 1 y " + limite + " hay " + contadorPares + " números pares.");
+
+            Console.WriteLine("\nPresiona cualquier tecla para salir...");
+            Console.ReadKey();
+        }
+    }
+}
