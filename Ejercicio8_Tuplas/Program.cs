55496673b3e4fd59709c336cc650f688b100302d
ashlyramosmendoza2202 <ashlyramosmendoza2202@gmail.com>
2026-02-26 20:41:03 -0400

    cargando

---
+++ Ejercicio8_Tuplas/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Escribe algo para contar sus letras: ");
+        string entrada = Console.ReadLine();
+
+        int largo = ObtenerLongitud(entrada);
+
+        Console.WriteLine($"La cadena tiene {largo} caracteres.");
+    }
+
+    static int ObtenerLongitud(string texto)
+    {
+        return texto.Length;
+    }
+}
