a0041d8b44ad14a9d4b1234ea3e1e9f306f7ef23
ashlyramosmendoza2202 <ashlyramosmendoza2202@gmail.com>
2026-02-26 22:30:16 -0400

    cargando

---
+++ Ejercicio_16/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Runtime.CompilerServices; 
+
+class Program
+{
+    static void Main()
+    {
+        ITuple tupla = (10, 20, 30, 40, 50);
+        Console.WriteLine("La longitud de la tupla es: " + tupla.Length);
+    }
+}
