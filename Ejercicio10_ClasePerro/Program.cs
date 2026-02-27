485f26ca4353d31283a79ea3574e8dd8fde68288
ashlyramosmendoza2202 <ashlyramosmendoza2202@gmail.com>
2026-02-26 21:59:49 -0400

    cargando

---
+++ Ejercicio10_ClasePerro/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+class Perro
+{
+    public string raza;
+
+    public void ImprimirRaza()
+    {
+        Console.WriteLine("La raza es: " + raza);
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        Perro miPerro = new Perro();
+        miPerro.raza = "Chihuahua";
+        miPerro.ImprimirRaza();
+    }
+}
