7b942e9c1a243c58f6a2c16315bb0517e5e30b62
ashlyramosmendoza2202 <ashlyramosmendoza2202@gmail.com>
2026-02-26 22:18:42 -0400

    cargando

---
+++ Ejercicio14_/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        var frutas = ("Manzana", "Pera", "Uva");
+        var verduras = ("Lechuga", "Zanahoria", "Tomate");
+
+        var mercado = (frutas.Item1, frutas.Item2, frutas.Item3, verduras.Item1, verduras.Item2, verduras.Item3);
+
+        Console.WriteLine("Tupla concatenada: " + mercado);
+    }
+}
