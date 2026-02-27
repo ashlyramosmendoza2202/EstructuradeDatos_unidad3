84a2953f0995dff6501e62eef2570abaa5a8f33e
ashlyramosmendoza2202 <ashlyramosmendoza2202@gmail.com>
2026-02-26 20:32:23 -0400

    cargando

---
+++ Ejercicio6_Tuplas/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Introduce los grados Celsius: ");
+        double celsius = Convert.ToDouble(Console.ReadLine());
+
+        double fahrenheit = ConvertirAFahrenheit(celsius);
+
+        Console.WriteLine($"{celsius}°C equivalen a {fahrenheit}°F");
+    }
+    static double ConvertirAFahrenheit(double c)
+    {
+        return (c * 9 / 5) + 32;
+    }
+}
