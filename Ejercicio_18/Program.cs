3a208e96c8f7c1809cb8bb508d541a5d90bed6bc
ashlyramosmendoza2202 <ashlyramosmendoza2202@gmail.com>
2026-02-26 22:36:53 -0400

    cargando

---
+++ Ejercicio_18/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Linq; 
+
+class Program
+{
+    static void Main()
+    {
+        var numeros = (1, 2, 2, 3, 2, 4);
+        int objetivo = 2;
+
+        int total = new[] { numeros.Item1, numeros.Item2, numeros.Item3, numeros.Item4, numeros.Item5, numeros.Item6 }
+                    .Count(n => n == objetivo);
+
+        Console.WriteLine($"El número {objetivo} aparece {total} veces.");
+    }
+}
