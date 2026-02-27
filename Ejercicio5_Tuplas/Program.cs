617f13747d256098d41112c54415dfee5abbe2c5
ashlyramosmendoza2202 <ashlyramosmendoza2202@gmail.com>
2026-02-26 20:25:58 -0400

    cargando

---
+++ Ejercicio5_Tuplas/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Escribe una palabra o frase: ");
+        string texto = Console.ReadLine();
+
+        int cantidadVocales = ContarVocales(texto);
+
+        Console.WriteLine($"El número total de vocales es: {cantidadVocales}");
+    }
+
+    static int ContarVocales(string frase)
+    {
+        int contador = 0;
+        string fraseMinuscula = frase.ToLower();
+
+        foreach (char letra in fraseMinuscula)
+        {
+            if ("aeiouáéíóú".Contains(letra))
+            {
+                contador++;
+            }
+        }
+
+        return contador;
+    }
+}
