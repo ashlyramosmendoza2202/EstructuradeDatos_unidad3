2a8b0aebb8d81ef98da4c4b93a56cd7011e4f268
ashlyramosmendoza2202 <ashlyramosmendoza2202@gmail.com>
2026-02-26 20:37:46 -0400

    cargando

---
+++ Ejercicio7_Tuplas/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        List<int> numeros = new List<int> { 5, 12, 8, 19, 33 };
+
+        Console.Write("Introduce el número que buscas: ");
+        int busqueda = Convert.ToInt32(Console.ReadLine());
+
+        if (ExisteEnLista(numeros, busqueda))
+        {
+            Console.WriteLine($"¡Sí! El {busqueda} está en la lista.");
+        }
+        else
+        {
+            Console.WriteLine($"No, el {busqueda} no se encuentra en la lista.");
+        }
+    }
+    static bool ExisteEnLista(List<int> lista, int numero)
+    {
+        return lista.Contains(numero);
+    }
+}
