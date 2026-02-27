7ee1f32a484b38682de5b15a334a06918beed701
ashlyramosmendoza2202 <ashlyramosmendoza2202@gmail.com>
2026-02-26 22:34:32 -0400

    cargando

---
+++ Ejercicio_17/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main()
+    {
+        var ciudades = ("Madrid", "Bogotá", "CDMX", "Lima", "Santiago");
+
+        List<string> listaCiudades = new List<string> {
+            ciudades.Item1, ciudades.Item2, ciudades.Item3, ciudades.Item4, ciudades.Item5
+        };
+
+        Console.WriteLine("Primer elemento de la lista: " + listaCiudades[0]);
+    }
+}
