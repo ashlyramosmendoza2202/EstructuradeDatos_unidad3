5f10abfabd49e93ac75a4a2da5359144d0fa51af
ashlyramosmendoza2202 <ashlyramosmendoza2202@gmail.com>
2026-02-26 22:09:35 -0400

    cargando

---
+++ Ejercicio12_ClaseLibro/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+class Libro
+{
+    public string titulo;
+
+    public void ImprimirTitulo()
+    {
+        Console.WriteLine("Título del libro: " + titulo);
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        Libro miLibro = new Libro();
+        miLibro.titulo = "Maria la Ciguapa";
+        miLibro.ImprimirTitulo();
+    }
+}
