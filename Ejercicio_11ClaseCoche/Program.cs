2f4d87ba0f845d4b8589c595c5c00583dd084002
ashlyramosmendoza2202 <ashlyramosmendoza2202@gmail.com>
2026-02-26 22:06:53 -0400

    cargando

---
+++ Ejercicio_11ClaseCoche/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+class Coche
+{
+    public string color;
+
+    public string ObtenerColor()
+    {
+        return color;
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        Coche miCoche = new Coche();
+        miCoche.color = "Azul";
+        Console.WriteLine("El color del coche es: " + miCoche.ObtenerColor());
+    }
+}
